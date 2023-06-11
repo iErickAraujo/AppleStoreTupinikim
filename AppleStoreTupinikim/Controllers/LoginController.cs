05c756d3203b821fe2f9ca1abb0cf16025f0b0d2
Gbritoo <gb785159@gmail.com>
2023-06-11 17:58:10 -0300

    teste branch Gustavo

---
+++ AppleStoreTupinikim/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppleStoreTupinikim.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
