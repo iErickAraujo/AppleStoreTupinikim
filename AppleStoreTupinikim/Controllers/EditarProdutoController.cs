36f5242c92bec144d95be94d8b7b5bd44c19ef18
iErickAraujo <erickaraujo4984@gmail.com>
2023-06-13 00:11:41 -0300

    Editar parcialmente funcionando

--- AppleStoreTupinikim/Controllers/EditarProdutoController.cs
+++ AppleStoreTupinikim/Controllers/EditarProdutoController.cs
@@ -1,7 +1,17 @@
-﻿using FireSharp.Config;
-using FireSharp.Interfaces;
+﻿using Microsoft.AspNetCore.Mvc;
+
+//importação das bibliotecas
 using FireSharp;
-using Microsoft.AspNetCore.Mvc;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+using Newtonsoft.Json;
+//
+//importa o modelo de classe
+using AppleStoreTupinikim.Models;
+//importa os controllers do projeto
+using AppleStoreTupinikim.Controllers;
 
 namespace AppleStoreTupinikim.Controllers
 {
@@ -23,5 +33,38 @@ namespace AppleStoreTupinikim.Controllers
 
         }
         //
+        public IActionResult EditarProduto(string idproduto)
+        {
+            //Get Busca no banco pelo Idproduto
+            FirebaseResponse response = cliente.Get("Produtos/"+ idproduto);
+            //para obter o produto//convert o conteudo para classe produtos
+            Produtos produto = response.ResultAs<Produtos>();
+            //atualizar o produto.IdProduto com idproduto(que foi recebido)
+            //pois no banco não existe id dentro dos valores
+            //somente Nome, Valor e Estoque;
+            produto.IdProduto = idproduto;
+            return View(produto);
+        }
+        //metodo para editar
+        [HttpPost]
+        public IActionResult EditarProduto(Produtos oProduto)
+        {
+            //armazenar o idProduto temporariamente
+            string idproduto=oProduto.IdProduto;
+            //pois não queremos que o id entre nos valores do banco
+            //sendo assim não inserir
+            oProduto.IdProduto = null;
+
+            //Update//passa oProduto para ser atualizado
+            //dessa forma estamos dizendo que o idproduto vai atualizar seus dados
+            FirebaseResponse response = cliente.Update("Produtos/" + idproduto, oProduto);
+            //se esta tudo ok retorna atualiza o produto
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                //retorna para a view lista de produtos
+                return RedirectToAction("ListaDeProdutos", "ListaDeProdutos");
+            }
+            else { return View(); }
+        }
     }
 }
